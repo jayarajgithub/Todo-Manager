b2b8b6712c59ee574088e07f491807b8e7838575
jayarajgithub <49501678+jayarajgithub@users.noreply.github.com>
2021-11-28 19:32:26 +0530

    update the project

---
+++ test/channels/application_cable/connection_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
+  # test "connects with cookies" do
+  #   cookies.signed[:user_id] = 42
+  #
+  #
+  # end
+end
